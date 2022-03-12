e17e98bbffbcdea27d1936ff9a0c989b8c95e153
Alex Salazar <alexwsalazar@gmail.com>
2022-03-12 16:40:18 -0600

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,41 @@
+var saveButton = $("#savefunct");
+
+var messageText = $('.col-10 textarea');
+
+
+var alertButtonEl = $('#alert-btn');
+
+var arr = ["car","bus","plane",];
+
+
+renderLastRegistered();
+console.log(renderLastRegistered);
+// function displayMessage(type, message){
+//  messageText.textContent = message;
+//  messageText.setAttribute('class', type);
+// }
+
+function renderLastRegistered(){
+    var task = localStorage.getItem('task',);
+    
+    messageText.textContent = task;
+    
+};
+
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+
+
+saveButton.on("click", function(){
+    var task = document.getElementsByClassName('.col-10 textarea').value;
+    localStorage.setItem("task", task);
+});
+
+
+
+
+
+
+
+
